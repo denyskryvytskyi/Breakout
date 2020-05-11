e175a831b9fb758e1044b13280ec88ccc1cd170e
Denys Kryvytskyi <kryvytskyi.denys@gmail.com>
2020-05-11 18:16:43 +0300

    Added core libs and premake
    * added glad, glfw and glm libraries
    * premake and generated projects

---
+++ premake5.lua
@@ -0,0 +1,51 @@
+workspace "Breakout"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Breakout"
+    location "Breakout"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/src/**.c"
+    }
+
+    includedirs
+    {
+        "vendor/glad/include",
+        "vendor/GLFW/include",
+        "vendor/glm/include"
+    }
+
+    libdirs
+    {
+        "vendor/GLFW/lib"
+    }
+
+    links
+    {
+        "glfw3.lib",
+        "opengl32.lib"
+    }
+
+    filter "configurations:Debug"
+		defines "BO_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BO_RELEASE"
+		optimize "On"
